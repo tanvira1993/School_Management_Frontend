19aa15e21dceefa603bca00972b90d84c424bc6d
tanvira <tanvira1993@gmail.com>
2020-05-07 16:58:34 +0600

    login page css fix

--- templates/login-template.js
+++ templates/login-template.js
@@ -3,46 +3,44 @@ const LoginTemplate = `
 
 
 
-<div class="hold-transition login-page">
+<div class="hold-transition login-page" style="background-color:#fff">
 <div class="login-box">
-  <div class="login-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
-  </div>
-  <!-- /.login-logo -->
-  <div class="login-box-body">
-    <p class="login-box-msg">Sign in to start your session</p>
+<div class="login-logo">
+<a href="../../index2.html"><b>Login</b></a>
+</div>
+<!-- /.login-logo -->
+<div class="login-box-body" style="background-color:#b2beb5">
+<p class="login-box-msg">Sign in to start your session</p>
 
 
-      <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Email" v-model="user.email">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password" v-model="user.password">
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      </div>
-      <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox"> Remember Me
-            </label>
-          </div>
-        </div>
-        <!-- /.col -->
-        <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat" @click="login()">Sign In</button>
-        </div>
-        <!-- /.col -->
-      </div>
+<div class="form-group has-feedback">
+<input type="email" class="form-control" placeholder="Email" v-model="user.email">
+<span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+</div>
+<div class="form-group has-feedback">
+<input type="password" class="form-control" placeholder="Password" v-model="user.password">
+<span class="glyphicon glyphicon-lock form-control-feedback"></span>
+</div>
+<div class="row">
+<div class="col-xs-8">
+<div class="checkbox icheck">
 
-    
+</div>
+</div>
+<!-- /.col -->
+<div class="col-xs-4">
+<button type="submit" class="btn btn-primary btn-block btn-flat" @click="login()">Sign In</button>
+</div>
+<!-- /.col -->
+</div>
 
-    <a href="#">I forgot my password</a><br>
-    <a href="register.html" class="text-center">Register a new membership</a>
 
-  </div>
-  <!-- /.login-box-body -->
+
+<a href="#">I forgot my password</a><br>
+<a href="register.html" class="text-center">Register a new membership</a>
+
+</div>
+<!-- /.login-box-body -->
 </div>
 
 
